024dccdffd460313aa6013d65832dfe1b2d8a83e
Dodi Devrian Andrianto <84231458+DodiDevrian@users.noreply.github.com>
2026-07-09 19:59:16 +0700

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('M_auth', 'M_materi' ,'m_praktikan');
+$autoload['model'] = array('M_auth', 'M_materi' ,'M_praktikan');
